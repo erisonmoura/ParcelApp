959e91dc39c09b09f0455c26df8306806a44af61
erisonmoura <erisonmoura@gmail.com>
2014-04-17 10:15:18 -0300

    correcao

--- parcelApp/src/com/parcelinc/parcelapp/db/PagamentoDataBase.java
+++ parcelApp/src/com/parcelinc/parcelapp/db/PagamentoDataBase.java
@@ -158,7 +158,7 @@ public class PagamentoDataBase implements DataBase<Pagamento> {
 		double valor = c.getDouble(4);
 
 		Despesa despesa = null;
-		if (idDespesa != 0) {
+		if (idDespesa != null) {
 			despesa = despesaDataBase.get(idDespesa);
 		}
 		Usuario usuario = null;
